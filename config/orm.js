67a62c0e5190e24a2cebce3baa40f5cdf108ed8a
Josh Loftis <jshlfts32@gmail.com>
2017-11-14 13:21:24 -0500

    Additional css tweaks and cleaning up

--- config/orm.js
+++ config/orm.js
@@ -1,14 +1,14 @@
 const connection = require('./connection');
 
 const help = {
-  qMarks: (num) => {
+  qMarks(num) {
     let arr = [];
     for (let i = 0; i < num; i++) {
       arr.push("?");
     }
     return arr.toString();
   },
-  objToSql: (obj) => {
+  objToSql(obj) {
     let arr = [];
     for (var key in obj) {
       let value = obj[key];
@@ -24,28 +24,28 @@ const help = {
 };
 
 const orm = {
-  selectAll: (table, cb) => {
+  selectAll(table, cb) {
     let query = `SELECT * FROM ${table};`;
     connection.query(query, function(err, result){
       if (err) throw err;
       cb(result);
     });
   },
-  insertOne: (table, cols, vals, cb) => {
+  insertOne(table, cols, vals, cb) {
     let query = `INSERT INTO ${table} (${cols.toString()}) VALUES (${help.qMarks(vals.length)})`;
     connection.query(query, vals, function(err, result){
       if (err) throw err;
       cb(result);
     });
   },
-  updateOne: (table, objColVals, condition, cb) => {
+  updateOne(table, objColVals, condition, cb) {
     let query = `UPDATE ${table} SET ${help.objToSql(objColVals)} WHERE ${condition}`;
     connection.query(query, function(err, result){
       if (err) throw err;
       cb(result);
     });
   },
-  deleteOne: (table, condition, cb) => {
+  deleteOne(table, condition, cb) {
     let query = `DELETE FROM ${table} WHERE ${condition}`;
     connection.query(query, function(err, result){
       if (err) throw err;
--- models/burger.js
+++ models/burger.js
@@ -1,22 +1,22 @@
 const orm = require('../config/orm');
 
 const burger = {
-  all: (cb) => {
+  all(cb) {
     orm.selectAll('burgers', function(res){
       cb(res);
     });
   },
-  insert: (cols, vals, cb) => {
+  insert(cols, vals, cb) {
     orm.insertOne('burgers', cols, vals, function(res){
       cb(res);
     });
   },
-  update: (objColVals, condition, cb) => {
+  update(objColVals, condition, cb) {
     orm.updateOne('burgers', objColVals, condition, function(res) {
       cb(res);
     });
   },
-  delete: (condition, cb) => {
+  delete(condition, cb) {
     orm.deleteOne('burgers', condition, function(res){
       cb(res);
     });
