e8998cd15e6f4da8764b302bb37f85553d031c83
Josh Loftis <jshlfts32@gmail.com>
2018-01-08 20:57:22 -0500

    Eslint changes

--- public/assets/js/burgers-front.js
+++ public/assets/js/burgers-front.js
@@ -1,36 +1,36 @@
-$(function(){
-  $('.devour-btn').on('click', function(){
-    let id = $(this).data('id');
-    let isDevoured = {
-      devoured: 1
+$(() => {
+  $('.devour-btn').on('click', function update() {
+    const id = $(this).data('id');
+    const isDevoured = {
+      devoured: 1,
     };
     console.log(id);
-    $.ajax('/api/burger/' + id, {
+    $.ajax(`/api/burger/${id}`, {
       method: 'PUT',
-      data: isDevoured
-    }).then(function(){
+      data: isDevoured,
+    }).then(() => {
       location.reload();
     });
   });
 
-  $('#add-burger').on('click', function(){
-    let newBurger = {
+  $('#add-burger').on('click', () => {
+    const newBurger = {
       name: $('#text-area').val().trim(),
-      devoured: 0
+      devoured: 0,
     };
-    $.ajax(`/api/burger`, {
+    $.ajax('/api/burger', {
       method: 'POST',
-      data: newBurger
-    }).then(function(){
+      data: newBurger,
+    }).then(() => {
       location.reload();
     });
   });
 
-  $('.delete-btn').on('click', function(){
-    let id = $(this).data('id');
-    $.ajax('/api/burger/' + id, {
-      method: 'DELETE'
-    }).then(function(){
+  $('.delete-btn').on('click', function deleteBurger() {
+    const id = $(this).data('id');
+    $.ajax(`/api/burger/${id}`, {
+      method: 'DELETE',
+    }).then(() => {
       location.reload();
     });
   });
